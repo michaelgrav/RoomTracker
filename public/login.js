ef3b4e29ea2245dac24622fb3edf0b91ab6ca478
mdg200001 <29133471+michaelgrav@users.noreply.github.com>
2023-04-08 13:11:57 -0500

    Added login page

--- public/login.js
+++ public/login.js
@@ -1,13 +1,37 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {  
+  authDomain: "cs3354-roomtracker.firebaseapp.com",
+  projectId: "cs3354-roomtracker",
+  storageBucket: "cs3354-roomtracker.appspot.com",
+  messagingSenderId: "802621464013",
+  appId: "1:802621464013:web:717e234765bd061fddb884"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js';
 
 const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+
+export function login(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      localStorage.setItem("userUID", user.uid);
+      localStorage.setItem("userEmail", user.email);
+      window.location = "index.html";
+      // ...
+    })
+    .catch((error) => {
+      console.log("Invalid email or password")
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
